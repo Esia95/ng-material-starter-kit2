2db50cc2310357d1412177ada5a44689c0f9aaac
Estera Nowak <esteranowak95@gmail.com>
2022-11-25 14:53:46 +0100

    create product form

---
+++ src/app/components/product-form/product-form.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ProductFormComponent } from './product-form.component';
+
+@NgModule({
+  imports: [],
+  declarations: [ProductFormComponent],
+  providers: [],
+  exports: [ProductFormComponent]
+})
+export class ProductFormComponentModule {
+}
---
+++ src/app/components/product-form/product-form.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-product-form',
+  styleUrls: ['./product-form.component.scss'],
+  templateUrl: './product-form.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductFormComponent {
+}
